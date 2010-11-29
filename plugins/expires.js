b8fe231a728090b397e0003e09c46c11cceaf883
Greg Reimer <gregreimer@gmail.com>
2010-11-29 15:08:44 -0700

    added logic for cache-control header

--- plugins/expires.js
+++ plugins/expires.js
@@ -5,10 +5,11 @@ http://github.com/greim
 */
 
 /**
-Plugin to return an expires header.
+Plugin to return expiry/cache-control headers.
 usage: @expires(days, hours, minutes, seconds)
-example: @expires(4,3,2,1) will send an expires header causing the response to
-expire 4 days, 3 hours, two minutes, and one second from now.
+example: @expires(4,3,2,1) will send expiry headers causing the response to
+expire 4 days, 3 hours, two minutes, and one second from the time of the
+request.
 */
 
 exports.run = function(api) {
@@ -18,22 +19,24 @@ exports.run = function(api) {
 	var minutes = parseInt(api.arg(2)) || 0;
 	var seconds = parseInt(api.arg(3)) || 0;
 
-	var ms = 0;
+	var secs = 0;
 
-	ms += days    * 1000 * 60 * 60 * 24;
-	ms += hours   * 1000 * 60 * 60;
-	ms += minutes * 1000 * 60;
-	ms += seconds * 1000;
+	secs += days    * 60 * 60 * 24;
+	secs += hours   * 60 * 60;
+	secs += minutes * 60;
+	secs += seconds;
 
 	var expires = new Date();
-	expires.setTime(expires.getTime() + ms);
+	expires.setTime(expires.getTime() + secs * 1000);
 
 	var ri = api.getResponseInfo();
 
-	if (ms > 0) {
+	if (secs > 0) {
 		ri.headers.expires = expires.toUTCString();
+		ri.headers['cache-control'] = 'max-age='+secs;
 	} else {
 		delete ri.headers.expires;
+		delete ri.headers['cache-control'];
 	}
 
 	api.notify();
