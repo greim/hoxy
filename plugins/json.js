567e7fc5696781b57e8d59f0c2e7fe39efe0b992
greim <gr@wayin.com>
2013-11-19 12:24:45 -0700

    removed old files; this branch is a total rewrite

--- plugins/json.js
+++
@@ -1,9 +0,0 @@
-/*
-Written by Greg Reimer
-Copyright (c) 2011
-http://github.com/greim
-*/
-
-exports.run = function(api){
-	api.notify(new Error('Plugin EOL, use json-script or json-eval istead.'));
-};
