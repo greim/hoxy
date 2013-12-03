7e78a341f1193127e0962b054870c0def75c6dca
greim <gr@wayin.com>
2013-12-03 09:31:52 -0700

    combined mitm and proxy classes, combined hoxy-api and transaction classes

--- hoxy.js
+++ hoxy.js
@@ -4,7 +4,7 @@
  */
 
 var Logger = require('./lib/logger');
-var Mitm = require('./lib/mitm');
+var Proxy = require('./lib/mitm');
 var Base = require('./lib/base');
 var StreamBrake = require('./lib/stream-brake');
 var _ = require('lodash-node');
@@ -12,45 +12,11 @@ var events = require('events');
 
 // ---------------------------
 
-var Proxy = Base.extend(function(opts){
-  opts = _.extend({
-    port: 8080
-  }, opts);
-  this._mitm = new Mitm(opts);
-  this._mitm.on('log', function(item){
-    this.emit('log', item);
-  }.bind(this));
-  this._ghosts = {};
-  if (opts.log){
-    this.log(opts.log);
-  }
-},{
-  intercept: function(phase, cb){
-    this._mitm.intercept(phase, cb);
-  },
-  reset: function(){
-    this._mitm.reset();
-  },
-  close: function(){
-    this._mitm.close();
-    Object.keys(this._ghosts).forEach(function(key){
-      this._ghosts[key].close();
-    }.bind(this));
-  },
-  log: function(level){
-    var logger = new Logger(level);
-    this.on('log', function(log){
-      var message = log.message;
-      if (log.error){
-        message+='\n'+log.error.stack;
-      }
-      logger[log.level](message);
-    });
-  }
-});
-
 module.exports = {
   start: function(opts){
+    opts = _.extend({
+      port: 8080
+    }, opts);
     return new Proxy(opts);
   },
   forever: function(cb){
