939a3a7f6ae0acad012e189ff2ccbab5277b07d4
greim <gr@wayin.com>
2014-02-12 18:37:26 -0700

    Split up tests into multiple files

---
+++ test/serializer-tests.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2013 by Greg Reimer <gregreimer@gmail.com>
+ * MIT License. See mit-license.txt for more info.
+ */
+
+// MOCHA TESTS
+// http://visionmedia.github.com/mocha/
+
+var assert = require('assert')
+var serializer = require('../lib/serializer')
+
+// ---------------------------
+
+describe('serializer', function(){
+
+  it('should work', function(done){
+    var callCount = 0,
+    arr = [0,0,0,0]
+    serializer.serialize(arr, function(item, next){
+      callCount++
+      setTimeout(next, 0)
+    })
+    .then(function(){
+      assert.strictEqual(arr.length, callCount)
+      done()
+    })
+    .catch(function(err){
+      done(err)
+    })
+  })
+})
