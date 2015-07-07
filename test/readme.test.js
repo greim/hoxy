881736e3a7a7e1315fc191ddf5b5f9f6dce0382e
Greg Reimer <gregreimer@gmail.com>
2015-07-06 20:22:00 -0600

    renamed test files

---
+++ test/readme.test.js
@@ -0,0 +1,7 @@
+# Testing Hoxy
+
+To run all tests, stand anywhere inside of the project and do:
+````bash
+npm test
+````
+This will launch a suite of self-tests using [mocha](http://visionmedia.github.io/mocha/).
