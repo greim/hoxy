6ac96c9f1c050442ac75eb1d9b6b57b8b5f26c09
Greg Reimer <gregreimer@gmail.com>
2015-07-06 20:42:12 -0600

    More tweaks

--- test/lib/megabyte-stream.js
+++ test/lib/megabyte-stream.js
@@ -3,14 +3,14 @@
  * MIT License. See mit-license.txt for more info.
  */
 
-var streams = require('../../lib/streams')
-var Readable = require('string-stream')
+import streams from '../../lib/streams'
+import Readable from 'string-stream'
 
 // ---------------------------
 
-var hex = '0123456789abcdef'
-var kb = []
-for (var i=0; i<64; i++)
+const hex = '0123456789abcdef'
+let kb = []
+for (let i=0; i<64; i++)
   kb.push(hex)
 kb = kb.join('')
 
@@ -18,9 +18,10 @@ kb = kb.join('')
  * Return a readable stream from which can
  * be read a megabyte of dummy data.
  */
-module.exports = function(){
-  var result = []
-  for (var i=0; i<1000; i++)
+export default function() {
+  const result = []
+  for (let i=0; i<1000; i++) {
     result.push(kb)
+  }
   return new Readable(result.join(''))
 }
