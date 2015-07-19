6a90a0aeb25e8fe17f013d8cf6703260f42cc806
Greg Reimer <gregreimer@gmail.com>
2015-07-19 15:25:59 -0600

    added compile and prepublish scripts

--- hoxy.js
+++ hoxy.js
@@ -1,28 +1,23 @@
 /*
- * Copyright (c) 2014 by Greg Reimer <gregreimer@gmail.com>
+ * Copyright (c) 2015 by Greg Reimer <gregreimer@gmail.com>
  * MIT License. See mit-license.txt for more info.
  */
 
-require('babel/register')()
-require('babel/polyfill')
-
-var Proxy = require('./lib/proxy');
-
-// ---------------------------
+var Proxy = require('./lib/proxy')
 
 module.exports = {
   Proxy: Proxy,
-  forever: function(handler, ctx){
-    process.on('uncaughtException', function(err){
-      if (handler === undefined){
-        console.log(err.stack);
-      } else if (typeof handler === 'function'){
-        handler.call(ctx, err);
-      } else if (typeof handler.write === 'function'){
-        handler.write(err.stack);
+  forever: function(handler, ctx) {
+    process.on('uncaughtException', function(err) {
+      if (handler === undefined) {
+        console.log(err.stack)
+      } else if (typeof handler === 'function') {
+        handler.call(ctx, err)
+      } else if (typeof handler.write === 'function') {
+        handler.write(err.stack)
       } else {
-        console.log(err.stack);
+        console.log(err.stack)
       }
-    });
+    })
   },
-};
+}
